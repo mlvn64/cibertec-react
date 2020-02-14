de1e874bf94b3acf6ef295811215ae0be6630ab0
ivan.taype <ivan.taype@gmail.com>
2020-02-13 21:47:28 -0500

    Updates

---
+++ precache-manifest.e5fe865ec5cbf8d86664f7379b2f014e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871595bb174a0b069fdeba6c3640c2ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce45be7be4564feca5e",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "288dcdd29267895af2e2",
+    "url": "/static/css/main.12e04618.chunk.css"
+  },
+  {
+    "revision": "5ce45be7be4564feca5e",
+    "url": "/static/js/2.074ebaf5.chunk.js"
+  },
+  {
+    "revision": "dd897ee3cfa67e5c2ebba71b5eb1cda6",
+    "url": "/static/js/2.074ebaf5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "288dcdd29267895af2e2",
+    "url": "/static/js/main.e31318c1.chunk.js"
+  },
+  {
+    "revision": "bfacc7b9489dcb3e5d2b",
+    "url": "/static/js/runtime-main.b1b7ed8c.js"
+  },
+  {
+    "revision": "a1e184b8b64d0a5850f0b9a8dad670f8",
+    "url": "/static/media/loading.a1e184b8.svg"
+  }
+]);
